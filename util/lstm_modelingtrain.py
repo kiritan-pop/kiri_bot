aa7a6d97589559528a8190f2c6b17a196e7a410d
きりたん <30888641+kiritan-pop@users.noreply.github.com>
2018-03-02 07:28:31 +0900

    update

--- util/lstm_modelingtrain.py
+++ util/lstm_modelingtrain.py
@@ -16,18 +16,18 @@ from time import sleep
 
 import tensorflow as tf
 from keras.backend import tensorflow_backend
-config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True, visible_device_list="0"))
+config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 session = tf.Session(config=config)
 tensorflow_backend.set_session(session)
 graph = tf.get_default_graph()
 
 #変更するとモデル再構築必要
-maxlen = 20
-GPUs = 1
+maxlen = 15
+GPUs = 4
 
 #いろいろなパラメータ
-batch_size = (1024+1024+1024+1024)*GPUs     #大きくすると精度が上がるけど、モデル更新が遅くなるよー！
-epochs = 10         #トレーニングの回数
+batch_size = 2048*GPUs     #大きくすると精度が上がるけど、モデル更新が遅くなるよー！
+epochs = 50         #トレーニングの回数
 # 同時実行プロセス数
 process_count = multiprocessing.cpu_count() - 1
 
@@ -43,11 +43,11 @@ def sample(preds, temperature=1.0):
 class TextGenerator(Sequence):
     def __init__(self, text_path, batch_size, maxlen, wl_chars):
         # コンストラクタ
-        self.text = open(text_path).read()
+        self.text = list(open(text_path).read())
         self.batch_size = batch_size
         self.maxlen = maxlen
-        self.deta_len = len(self.text) - maxlen - 1
-        self.sample_per_epoch = int(self.deta_len/self.batch_size)  #端数は切り捨て。端数分は・・・
+        deta_len = len(self.text) - maxlen - 1
+        self.sample_per_epoch = int(deta_len/self.batch_size)  #端数は切り捨て。端数分は・・・
         self.wl_chars = wl_chars
         self.char_indices = dict((c, i) for i, c in enumerate(self.wl_chars))
         self.indices_char = dict((i, c) for i, c in enumerate(self.wl_chars))
@@ -68,7 +68,7 @@ class TextGenerator(Sequence):
         y = np.zeros((len(sentences), len(self.wl_chars)), dtype=np.bool)
 
         for i, (sentence,next_char) in enumerate(zip(sentences,next_chars)):
-            for t, char in enumerate(list(sentence)):
+            for t, char in enumerate(sentence):
                 try:
                     x[i, t, self.char_indices[char]] = 1
                 except:
@@ -94,19 +94,19 @@ class TextGenerator(Sequence):
             print('----- Generating text after Epoch')
 
             start_index = random.randint(0, len(self.text) - self.maxlen - 1)
-            for diversity in [0.2, 0.5, 1.0, 1.2]:
+            for diversity in [0.25, 0.5, 0.75, 1.0]:
                 print()
                 print('----- diversity:', diversity)
 
                 generated = ''
                 sentence = self.text[start_index: start_index + self.maxlen]
-                print('----- Generating with seed: "' + sentence + '"')
+                print('----- Generating with seed: "' + ''.join(sentence) + '"')
                 sys.stdout.write(generated)
 
                 for i in range(50):
                     #print('debug1 i=%d' %i)
                     x_pred = np.zeros((1, self.maxlen, len(self.wl_chars)))
-                    for t, char in enumerate(list(sentence)):
+                    for t, char in enumerate(sentence):
                         try:
                             x_pred[0, t, self.char_indices[char]] = 1.
                         except:
@@ -118,17 +118,19 @@ class TextGenerator(Sequence):
                     next_char = self.indices_char[next_index]
 
                     generated += next_char
-                    sentence = sentence[1:] + next_char
-
+                    sentence = sentence[1:]
+                    sentence.append(next_char)
                     sys.stdout.write(next_char)
                     sys.stdout.flush()
         print()
 
 if __name__ == '__main__':
     #辞書読み込み
-    wl_chars = list(open('../dic/wl2.txt').read())
+    wl_chars = list(open('../dic/wl.txt').read())
     wl_chars.append(r'\n')
     wl_chars.sort()
+    p_model = None
+    optimizer = RMSprop()
 
     if len(sys.argv) < 3:
         print('パラメータ足りないよ！')
@@ -137,26 +139,32 @@ if __name__ == '__main__':
         # loading the model
         print('load model...')
         model = load_model(sys.argv[2])
+
+        if GPUs > 1:
+            p_model = multi_gpu_model(model, gpus=GPUs)
+            p_model.compile(loss='categorical_crossentropy', optimizer=optimizer)
     else:
         # build the model: a single LSTM
         print('Build model...')
         model = Sequential()
-        model.add(LSTM(512, return_sequences=True, input_shape=(maxlen, len(wl_chars))))  #4096
+        model.add(LSTM(256, return_sequences=True, input_shape=(maxlen, len(wl_chars))))
         model.add(Dropout(0.1))
-        model.add(LSTM(256, return_sequences=True))
+        model.add(LSTM(64, return_sequences=True))
         model.add(Dropout(0.1))
-        model.add(LSTM(128))
+        model.add(LSTM(32))
         model.add(Dropout(0.1))
         model.add(Dense(len(wl_chars)))
         model.add(Activation('softmax'))
 
         if GPUs > 1:
-            model = multi_gpu_model(model, gpus=GPUs)
+            p_model = multi_gpu_model(model, gpus=GPUs)
+            p_model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
-        optimizer = RMSprop()
         model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
     model.summary()
+    if GPUs > 1:
+        p_model.summary()
 
     start_idx = 0
     if len(sys.argv) == 4 :
@@ -166,14 +174,15 @@ if __name__ == '__main__':
     #print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
     generator = TextGenerator(sys.argv[1], batch_size, maxlen, wl_chars)
     with graph.as_default():
-        model.fit_generator(generator,
-                            epochs=epochs,
-                            #callbacks=[print_callback],
-                            verbose=1,
-                            #validation_data=None,
-                            #validation_steps=None,
-                            #class_weight=None,
-                            initial_epoch=start_idx,
-                            max_queue_size=process_count * 5,
-                            workers=process_count,
-                            use_multiprocessing=True)
+        if GPUs > 1:
+            m = p_model
+        else:
+            m = model
+
+        m.fit_generator(generator,
+                        epochs=epochs,
+                        verbose=1,
+                        initial_epoch=start_idx,
+                        max_queue_size=process_count ,
+                        workers=2,
+                        use_multiprocessing=True)
