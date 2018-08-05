9ea1599e952ea6c10c20b75fb7ae88fd61b8792d
きりたん <30888641+kiritan-pop@users.noreply.github.com>
2018-08-05 11:11:58 +0900

    update

--- cnn/cnn_model.py
+++ cnn/cnn_model.py
@@ -6,11 +6,6 @@ from tensorflow.python.keras.layers import GaussianNoise
 from tensorflow.python.keras import backend as tensorflow_backend
 from tensorflow.python.keras.layers import BatchNormalization
 
-# import tensorflow as tf
-# config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=False, visible_device_list="2"))
-# session = tf.Session(config=config)
-# tensorflow_backend.set_session(session)
-
 def cnn_model(labels):
     # include_top=Falseによって、モデルから全結合層を削除
     input_tensor = Input(shape=(299, 299, 3))
@@ -25,9 +20,9 @@ def cnn_model(labels):
     # top_model.add(Dense(1024,))
     # top_model.add(Activation("softmax"))
     # top_model.add(BatchNormalization(input_shape=xception_model.output_shape[1:]))
-    top_model.add(GaussianNoise(stddev=0.3,input_shape=xception_model.output_shape[1:]))
-    # top_model.add(Dropout(0.3))
-    top_model.add(Dense(len(labels)))
+    top_model.add(GaussianNoise(stddev=0.2,input_shape=xception_model.output_shape[1:]))
+    top_model.add(Dropout(0.3))
+    top_model.add(Dense(len(labels),input_shape=xception_model.output_shape[1:]))
     top_model.add(Activation("softmax"))
 
     # 全結合層を削除したモデルと上で自前で構築した全結合層を結合
@@ -36,6 +31,6 @@ def cnn_model(labels):
     # 図3における14層目までのモデル重みを固定（VGG16のモデル重みを用いる）
     # print('model.layers:',len(xception_model.layers))
     # xception_model.trainable = False
-    for layer in xception_model.layers[:-5]:
+    for layer in xception_model.layers[:-50]:
         layer.trainable = False
     return model
